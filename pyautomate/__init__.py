106de9d73461da4c4282c8f8fd2ffc5362d7b189
Lee Seongjoo <seongjoo@codebasic.co>
2017-04-14 08:10:53 +0900

    conform pycodestyle

--- pyautomate/__init__.py
+++ pyautomate/__init__.py
@@ -3,6 +3,7 @@ import shutil
 import subprocess
 from datetime import datetime
 
+
 class FileExistsWarning(UserWarning):
     def __init__(self, path, extra_msg=None):
         super().__init__(self)
@@ -15,15 +16,18 @@ class FileExistsWarning(UserWarning):
             msg = '{} {}'.format(msg, self.extra_msg)
         return msg
 
+
 def get_timestamp(date_time=None):
     if not date_time:
         date_time = datetime.now()
 
     return date_time.strftime('%Y-%m-%dT%H%M')
 
+
 def prompt_user(message):
     prompt = input('{} (y/n) '.format(message))
     return True if prompt.lower().startswith('y') else False
+
 
 def touch(filepath, content=None, overwrite=True, encoding='utf-8'):
     mode = 'w' if overwrite else 'a'
@@ -33,10 +37,12 @@ def touch(filepath, content=None, overwrite=True, encoding='utf-8'):
 
     return filepath
 
+
 def print_file(path, nlines=5, encoding='utf-8'):
     with open(path, encoding=encoding) as file:
         for line in file:
             print(line.rstrip())
+
 
 def delete_path(path, ask=True):
     if not os.path.exists(path):
@@ -53,16 +59,17 @@ def delete_path(path, ask=True):
 
     elif os.listdir(path):
         if ask:
-            confirm = prompt_user('{} is not empty! Delete anyway?'.format(path))
+            msg = '{} is not empty! Delete anyway?'
+            confirm = prompt_user(msg.format(path))
         if not confirm:
             return
         return shutil.rmtree(path)
+
 
 def zipit(output, files, option='c'):
     command = 'python -m zipfile'
     command = '{} -{} {}'.format(command, option, output)
     command = '{} {}'.format(
         command, ' '.join(files))
-        #command, ' '.join(map(lambda fn: '"{}"'.format(fn), files)))
     result = subprocess.run(command.split())
     result.check_returncode()
