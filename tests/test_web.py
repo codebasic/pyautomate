4aecea4fd2c0ecaca31e74c2f55f22df938864a3
Lee Seongjoo <seongjoo@codebasic.co>
2017-04-13 15:42:05 +0900

    new test added

---
+++ tests/test_web.py
@@ -0,0 +1,32 @@
+import os
+import pytest
+from pyautomate import web
+
+url = 'http://www.example.com'
+
+
+@pytest.fixture
+def webpage():
+    if not os.path.exists('example_com.html'):
+        web.download(url, 'example_com.html')
+    yield 'example_com.html'
+    os.unlink('example_com.html')
+
+
+def test_parse_html():
+    assert web.parse_html(url)
+    if not os.path.exists('example_com.html'):
+        web.download(url, 'example_com.html')
+    assert web.parse_html('example_com.html')
+
+    html = open('example_com.html', encoding='utf8').read()
+    assert web.parse_html(html)
+
+    with pytest.raises(ValueError):
+        web.parse_html([])
+
+
+def test_html(webpage):
+    html = web.parse_html(webpage)
+    assert html
+    assert html.title.text == 'Example Domain'
